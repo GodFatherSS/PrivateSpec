a02611155e11d0c00980a346ccad6f0393ae0f21
施澍 <shishu@shishudeMacBook-Pro.local>
2016-10-19 14:14:12 +0800

    v0.1.0

---
+++ BBQObject.h
@@ -0,0 +1,16 @@
+//
+//  BBQObject.h
+//  Pods
+//
+//  Created by 施澍 on 16/10/19.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BBQObject : NSObject
+
+- (void)sayHello;
+- (void)sayHaha;
+
+@end
---
+++ BBQObject.m
@@ -0,0 +1,21 @@
+//
+//  BBQObject.m
+//  Pods
+//
+//  Created by 施澍 on 16/10/19.
+//
+//
+
+#import "BBQObject.h"
+
+@implementation BBQObject
+
+- (void)sayHello {
+    NSLog(@"Hello!");
+}
+
+- (void)sayHaha {
+    NSLog(@"Haha!");
+}
+
+@end
